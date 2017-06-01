7ee0e4d414008bbcb82c5f3a622d5165961b2926
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2017-06-01 22:08:13 +0200

    mergen

---
+++ code/mergen.R
@@ -0,0 +1,24 @@
+# Mergen
+# Sat May 20 09:44:25 2017
+# Jan-Philipp Kolb
+
+?merge
+
+authors <- data.frame(
+  surname = I(c("Tukey", "Venables", "Tierney", "Ripley", "McNeil")),
+  nationality = c("US", "Australia", "US", "UK", "Australia"),
+  deceased = c("yes", rep("no", 4)))
+books <- data.frame(
+  name = I(c("Tukey", "Venables", "Tierney",
+             "Ripley", "Ripley", "McNeil", "R Core")),
+  title = c("Exploratory Data Analysis",
+            "Modern Applied Statistics ...",
+            "LISP-STAT",
+            "Spatial Statistics", "Stochastic Simulation",
+            "Interactive Data Analysis",
+            "An Introduction to R"),
+  other.author = c(NA, "Ripley", NA, NA, NA, NA,
+                   "Venables & Smith"))
+
+(m1 <- merge(authors, books, by.x = "surname", by.y = "name"))
+(m2 <- merge(books, authors, by.x = "name", by.y = "surname"))
